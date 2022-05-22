1a63f78952bebb13bccc34fa59adfbb73bdd39bf
khanoguz <45708619+khanoguz@users.noreply.github.com>
2022-05-23 00:24:40 +0300

    Add files via upload

---
+++ Dayy16- Coffee Machine with OOP/main.py
@@ -0,0 +1,34 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+flag = True
+
+choice = CoffeeMaker()
+current = MoneyMachine()
+menu1 = Menu()
+
+while flag:
+    text = "What would you like? (" + menu1.get_items() + ")"
+    order = input(text)
+    if order == "report":
+        choice.report()
+        current.report()
+
+    elif order == "off":
+        print("Maintance mod")
+        flag = False
+    else:
+        cost = menu1.find_drink(order)
+        if order == "espresso":
+            if choice.is_resource_sufficient(cost) and current.make_payment(cost.cost) == True:
+                choice.make_coffee(cost)
+
+        elif order == "latte":
+            if choice.is_resource_sufficient(cost) and current.make_payment(cost.cost):
+                choice.make_coffee(cost)
+
+        elif order == "cappuccino":
+            drink = Menu().menu[2]
+            if choice.is_resource_sufficient(cost) and current.make_payment(cost.cost):
+                choice.make_coffee(cost)
