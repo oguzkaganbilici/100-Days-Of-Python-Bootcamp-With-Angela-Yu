0a1c176ba0a5890d272ae0fcca159c9d13c56569
Oğuz Kağan <45708619+khanoguz@users.noreply.github.com>
2022-06-22 00:00:05 +0300

    Add files via upload

--- Dayy36, Stock News Monitoring Projects/main.py
+++ Dayy36, Stock News Monitoring Projects/main.py
@@ -2,15 +2,18 @@ STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 MY_API ="L9Q5Z1YBEWFN16RP"
 NEWS_API = "e800b82e702640ea96d26cfd2ef66bb5"
+MY_EMAIL = "delykurt@gmail.com"
+MY_PASSWORD = "soscqgolxpzqprsb"
+
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
-import requests
+
+import requests,smtplib
 
 respond = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={STOCK}&apikey={MY_API}")
+
 respond.raise_for_status()
-
 data_json = respond.json()
-
 
 data = data_json["Time Series (Daily)"]
 values = data.items()
@@ -18,20 +21,35 @@ values = data.items()
 yesterday = float(data[list(values)[1][0]]["4. close"])
 before_yesterday = float(data[list(values)[2][0]]["4. close"])
 
-print(yesterday, before_yesterday)
-if (yesterday - before_yesterday) >= (before_yesterday * 5/100) or (yesterday - before_yesterday) <= -(before_yesterday * 5/100):
-    print("news")
+if (yesterday - before_yesterday) >= (before_yesterday * 0.05) or (yesterday - before_yesterday) <= -(before_yesterday * 0.05):
+    ## STEP 2: Use https://newsapi.org
+    # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
+    news_respond = requests.get(f"https://newsapi.org/v2/everything?q={COMPANY_NAME}&from=2022-06-21&sortBy=popularity&apiKey={NEWS_API}")
+    news_respond.raise_for_status()
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-news_respond = requests.get(f"https://newsapi.org/v2/everything?q={COMPANY_NAME}&from=2022-06-21&sortBy=popularity&apiKey={NEWS_API}")
-news_respond.raise_for_status()
+    news_json = news_respond.json()
 
-news_json = news_respond.json()
+    messages = ""
+    for i in range(1, 4):
+        headline = news_json["articles"][i]["title"]
+        brief = news_json["articles"][i]["description"]
+        message = f"{headline}\n{brief}\n\n"
+        messages += message
 
-print(news_json["articles"][0]["description"])
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as con:
+        if yesterday >= before_yesterday:
+            subject = f"{STOCK}, {round((yesterday - before_yesterday) * 100 / before_yesterday, 2)}% UP"
+        else:
+            subject = f"{STOCK}, {round((before_yesterday - yesterday) * 100 / before_yesterday, 2  )}% DOWN"
+        con.starttls()
+        con.login(MY_EMAIL, MY_PASSWORD)
+        con.sendmail(from_addr=MY_EMAIL, to_addrs="oguzkaganbilici1@gmail.com", msg=f"Subject:{subject}\n{messages}")
+
+
+
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a seperate message with the percentage change and each article's title and description to your phone number.
 
 
 #Optional: Format the SMS message like this: 
