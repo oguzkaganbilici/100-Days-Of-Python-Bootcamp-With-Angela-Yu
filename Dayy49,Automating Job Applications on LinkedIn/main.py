1235d5502b62955de366243b857ffc5364adbabd
Oğuz Kağan <45708619+khanoguz@users.noreply.github.com>
2022-07-21 14:58:21 +0300

    Add files via upload

--- Dayy49,Automating Job Applications on LinkedIn/main.py
+++ Dayy49,Automating Job Applications on LinkedIn/main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
 import time
-url = "https://www.linkedin.com/jobs/search/?f_AL=true&geoId=101165590&keywords=Python%20Developer&location=Birle%C5%9Fik%20Krall%C4%B1k"
+url = "https://www.linkedin.com/jobs/search/?f_AL=true&geoId=101165590&keywords=C%2B%2B%20Developer&location=Birle%C5%9Fik%20Krall%C4%B1k"
 driver = webdriver.Chrome("C:\\Users\\061885\\Desktop\\Dayy49,Automating Job Applications on LinkedIn\Development\\chromedriver.exe")
 driver.get(url)
 login_button = driver.find_element(By.CLASS_NAME,'nav__button-secondary')
@@ -21,19 +21,71 @@ time.sleep(10)
 jobs = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
 for i in jobs:
     i.click()
-    time.sleep(5)
+    print("basvuru secildi")
+    time.sleep(3)
     try:
-        apply_button = driver.find_element(By.CLASS_NAME, 'jobs-s-apply')
-        if apply_button:
-            apply_button.click()
-            next_button = driver.find_element(By.CSS_SELECTOR,"button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
+        apply_button = driver.find_element(By.CLASS_NAME, "jobs-apply-button--top-card")
+        apply_button.click()
+        print("basvur tiklandi")
+        time.sleep(1)
+        next_flag = True
+        while next_flag:
+            try:
+                next_button = driver.find_element(By.CSS_SELECTOR, "button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
+                print("next bulundu")
+                next_button.click()
+                time.sleep(1)
+                try:
+                    radio_error = driver.find_element(By.CLASS_NAME, "fb-radio")
+                    close = driver.find_element(By.CLASS_NAME,"artdeco-modal__dismiss")
+                    close.click()
+                    time.sleep(1)
+                    dont_save = driver.find_element(By.CLASS_NAME,"artdeco-modal__confirm-dialog-btn")
+                    dont_save.click()
+                    time.sleep(1)
+                except:
+                    try:
+                        select_error = driver.find_element(By.CLASS_NAME, "fb-dropdown")
+                        close = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+                        close.click()
+                        time.sleep(1)
+                        dont_save = driver.find_element(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")
+                        dont_save.click()
+                        time.sleep(1)
+                    except:
+                        pass
+            except:
+                print("next bulunamadı")
+                next_flag = False
+
+    except NoSuchElementException:
+        pass
+
+
+""" 
+
+
+        while flag:
+            print("next buton bulundu")
+            time.sleep(2)
+            if next_button:
+                next_button.click()
+            else:
+                flag = False
+    else:
+        i.click()
+        time.sleep(3)
+
+
+
+print("basvuruya tıkladı")
+        time.sleep(5)
+        try:
+            
+            
+            
             while next_button:
                 next_button.click()
-
-
-
-"""
-
-while next_button:
-    next_button.click()
+                print("next butonuna tikladi")
+                errors = driver.find_element(By.CLASS_NAME, "fb-radio-buttons")
 """
