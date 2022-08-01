1ee5ed2f80c816af8d83e5901001910f9e86adf0
Oğuz Kağan <45708619+khanoguz@users.noreply.github.com>
2022-08-01 23:44:49 +0300

    Update main.py

--- Dayy53,Data Entry Job Automation - Web Scraping Capstone Project/main.py
+++ Dayy53,Data Entry Job Automation - Web Scraping Capstone Project/main.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 form_link = 'https://docs.google.com/forms/d/e/1FAIpQLSdOhLBd_TmbtIKnfb3c3lZvKrRsgFUmkfWu1-Dthbr3O4YZaA/viewform?usp=sf_link'
 property_link = "https://www.hepsiemlak.com/ankara-kiralik"
 
-drive = webdriver.Chrome('C:\\Users\\061885\\Desktop\\Dayy49,Automating Job Applications on LinkedIn\\Development\\chromedriver.exe')
+drive = webdriver.Chrome('chromedriver.exe')
 drive.get(property_link)
 links = drive.find_elements(By.CLASS_NAME, 'list-view-content')
 links_ = []
@@ -26,14 +25,27 @@ for i in address:
 for i in prices_:
     prices__.append(i[0])
 
-print(len(prices__))
-
-
 drive.get(form_link)
-time.sleep(5)
-answer_address = drive.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
-answer_address.send_keys(address_[0])
-answer_price = drive.find_element(By.XPATH,'//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-answer_price.send_keys(prices[0])
-answer_link = drive.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-answer_link.send_keys(links_[0])
+time.sleep(3)
+
+for i in range(0, 24):
+    answer_address = drive.find_element(By.XPATH,
+                                        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    answer_address.send_keys(address_[i])
+
+    answer_price = drive.find_element(By.XPATH,
+                                      '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    answer_price.send_keys(prices__[i])
+
+    answer_link = drive.find_element(By.XPATH,
+                                     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    answer_link.send_keys(links_[i])
+
+    send_button = drive.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+    send_button.click()
+    time.sleep(2)
+    send_another = drive.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    send_another.click()
+    time.sleep(1)
+
+drive.quit()
