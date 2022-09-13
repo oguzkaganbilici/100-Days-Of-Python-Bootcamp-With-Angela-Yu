c945ab8dcde4405101b76a5e1dd0af3b52148e62
Oğuz Kağan <45708619+khanoguz@users.noreply.github.com>
2022-09-13 23:54:59 +0300

    Add files via upload

--- Dayy68, Authentication with Flask/main.py
+++ Dayy68, Authentication with Flask/main.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
@@ -5,10 +6,17 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 
 app = Flask(__name__)
 
+
+
+
 app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
 
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
@@ -20,6 +28,11 @@ class User(UserMixin, db.Model):
 # db.create_all()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -29,21 +42,36 @@ def home():
 def register():
     if request.method == 'POST':
         name_ = request.form["name"]
-        new_user = User(email=request.form["email"], password=request.form["password"], name=request.form["name"])
+        pass_ = generate_password_hash(password=request.form['password'], method='pbkdf2:sha256', salt_length=8)
+        new_user = User(email=request.form["email"], password=pass_, name=name_)
         db.session.add(new_user)
         db.session.commit()
         return render_template("secrets.html", name_tag=name_)
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        user = User.query.filter_by(email=request.form["email"]).first()
+        if user:
+            if check_password_hash(user.password, request.form["password"]):
+                flash("Login successfully!")
+                login_user(user)
+                #logout_user(user) -> to log out
+                return redirect(url_for('secrets', name=user.name))
+            else:
+                flash("Wrong e-mail or password. Please try again.")
+        else:
+            flask.flash("That user does not exist. ")
     return render_template("login.html")
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    return render_template("secrets.html")
+    name_ = request.args.get("name")
+    return render_template("secrets.html", name_tag=name_, logged_in=True)
 
 
 @app.route('/logout')
