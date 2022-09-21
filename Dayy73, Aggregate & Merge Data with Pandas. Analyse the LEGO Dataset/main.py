8f07af70d46f942e750670db40d32b60abfac878
Oğuz Kağan <45708619+khanoguz@users.noreply.github.com>
2022-09-21 23:38:20 +0300

    Add files via upload

--- Dayy73, Aggregate & Merge Data with Pandas. Analyse the LEGO Dataset/main.py
+++ Dayy73, Aggregate & Merge Data with Pandas. Analyse the LEGO Dataset/main.py
@@ -53,6 +53,78 @@ ax1.set_xlabel("Year")
 ax1.set_ylabel("Number of sets", color="red")
 ax2.set_ylabel("Number of themes", color="blue")
 
+#%%
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+color_df = pd.read_csv("data/colors.csv")
+lego_df = pd.read_csv("data/sets.csv")
+
+parts_per_set = lego_df.groupby("year").agg({"num_parts":pd.Series.mean})
+
+plt.figure(figsize=(20,30))
+plt.xlabel("Years")
+plt.ylabel("Count of parts")
+plt.scatter(parts_per_set.index[:-2], parts_per_set.num_parts[:-2])
+plt.show()
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+color_df = pd.read_csv("data/colors.csv")
+lego_df = pd.read_csv("data/sets.csv")
+themes_df = pd.read_csv("data/themes.csv")
+
+
+set_theme_count = lego_df.theme_id.value_counts()
+
+set_theme_count_df = pd.DataFrame({"id":set_theme_count.index,  
+                                  "set_counts": set_theme_count.values}) #to make a new dataframe
+
+merged_df = pd.merge(set_theme_count_df, themes_df, on="id").head(10)
+
+
+plt.figure(figsize=(12,10))
+plt.xticks(fontsize=14, rotation=45)
+plt.yticks(fontsize=14)
+plt.xlabel("Themes")
+plt.ylabel("Number of Themes")
+plt.bar(merged_df.name, merged_df.set_counts)
+plt.show()
+
+"""
+                    SUMMARIZE
+In this lesson we looked at how to:
+
+use HTML Markdown in Notebooks, such as section headings # and how to embed images with the <img> tag.
+
+combine the groupby() and count() functions to aggregate data
+
+use the .value_counts() function
+
+slice DataFrames using the square bracket notation e.g., df[:-2] or df[:10]
+
+use the .agg() function to run an operation on a particular column
+
+rename() columns of DataFrames
+
+create a line chart with two separate axes to visualise data that have different scales.
+
+create a scatter plot in Matplotlib
+
+work with tables in a relational database by using primary and foreign keys
+
+.merge() DataFrames along a particular column
+
+create a bar chart with Matplotlib
+
+"""
+
+
+
 
 
 
